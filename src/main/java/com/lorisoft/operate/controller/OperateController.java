1ca8ab48a487ef330987b2d5da0c8f6ba4bed062
regan johnson <regan.johnson@points.com>
2020-08-03 12:46:55 -0400

    rough dashboard page layout

--- src/main/java/com/lorisoft/operate/controller/OperateController.java
+++ src/main/java/com/lorisoft/operate/controller/OperateController.java
@@ -25,6 +25,25 @@ private static final Logger log = LoggerFactory.getLogger(OperateController.clas
    // index page
    @RequestMapping(value = "/index")
    public String index(Model model) {
+    int towns = 6;
+    int rollingStock = 23;
+    int consignees = 3;
+    int dropZones = 10;
+    int carRequests = 12;
+    int trains = 14;
+     model.addAttribute("townCount", towns);
+     model.addAttribute("rollingStockCount", rollingStock);
+     model.addAttribute("consigneeCount", consignees);
+     model.addAttribute("dropZoneCount", dropZones );
+     model.addAttribute("carRequestCount", carRequests);
+     model.addAttribute("trainCount", trains);
+
       return "index";
+   }
+
+   // preferences page
+   @RequestMapping(value = "/preferences")
+   public String preferences(Model model) {
+      return "preferences";
    }
 }
---
+++ src/main/resources/static/menu.js
@@ -0,0 +1,24 @@
+
+  document.write('<td width="160" bgcolor="#4F76AD" height="0" valign="top">');
+
+  document.write('<a title="Equipment Register" href="equipment" class="toc">Equipment Register</a>');
+  document.write('<hr noshade size="1" color="#C0C0C0">');
+  document.write('<p class="tocEntry">');
+  document.write('<a title="Modules" href="modules" class="toc">Modules</a>');
+  document.write('<hr noshade size="1" color="#C0C0C0">');
+  document.write('<p class="tocEntry">');
+  document.write('<a title="Trains" href="trains" class="toc">Trains</a>');
+  document.write('<hr noshade size="1" color="#C0C0C0">');
+  document.write('<p class="tocEntry">');
+
+
+  document.write('<a title="Trainlists" href="trainlists" class="toc">TrainLists</a>');
+  document.write('<hr noshade size="1" color="#C0C0C0">');
+  document.write('<p class="tocEntry">');
+  document.write('<a title="Preferences" href="preferences" class="toc">Preferences</a>');
+  document.write('<hr noshade size="1" color="#C0C0C0">');
+  document.write('<p class="tocEntry">');
+  document.write('<a title="Dashboard" href="index" class="toc">Dashboard</a>');
+  document.write('<hr noshade size="1" color="#C0C0C0">');
+  document.write('<p class="tocEntry">');
+  document.write('</td>');
